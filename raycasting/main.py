62adcbd1d18d9fa8be59702e3afee29d1cc364b9
Juan Pablo Rivera <rivera.ruiz.juanp@gmail.com>
2024-12-18 00:03:18 -0600

    added random walking to the raycaster

--- raycasting/main.py
+++ raycasting/main.py
@@ -1,10 +1,10 @@
 import pygame
 import random
-import math
+import numpy as np
 
     
 def dist(p1, p2) -> float:
-    return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
+    return np.sqrt(np.pow(p1[0] - p2[0], 2) + np.pow(p1[1] - p2[1], 2))
 
 
 class Boundary:
@@ -32,39 +32,37 @@ class Ray:
 
 
     def cast(self, boundary: Boundary) -> tuple[float, float]:
-        try: 
-            # x = self.pos[0] + 5*math.cos(self.th)
-            # y = self.pos[1] + 5*math.sin(self.th)
-            # m = (y - self.pos[1])/(x - self.pos[0])
-            # b = self.pos[1] - m * self.pos[0]
+        # x = self.pos[0] + 5*math.cos(self.th)
+        # y = self.pos[1] + 5*math.sin(self.th)
+        # m = (y - self.pos[1])/(x - self.pos[0])
+        # b = self.pos[1] - m * self.pos[0]
 
-            # mb = (boundary.p1[1] - boundary.p2[1])/(boundary.p1[0] - boundary.p2[0])
-            # bb = boundary.p1[1] - mb * boundary.p1[0]
+        # mb = (boundary.p1[1] - boundary.p2[1])/(boundary.p1[0] - boundary.p2[0])
+        # bb = boundary.p1[1] - mb * boundary.p1[0]
 
-            # x_inter = int((bb - b)/(m - mb))
-            # y_inter = int(m * x_inter + b)
+        # x_inter = int((bb - b)/(m - mb))
+        # y_inter = int(m * x_inter + b)
 
-            # x_min, x_max = min(boundary.p1[0], boundary.p2[0]), max(boundary.p1[0], boundary.p2[0])
-            # y_min, y_max = min(boundary.p1[1], boundary.p2[1]), max(boundary.p1[1], boundary.p2[1])
-            # if x_min <= x_inter <= x_max and y_min <= y_inter <= y_max:
-            #     return x_inter, y_inter
-            # return None
-        
-            x1, y1 = self.pos
-            x2 = x1 + math.cos(self.th)
-            y2 = y1 + math.sin(self.th)
-            x3, y3 = boundary.p1
-            x4, y4 = boundary.p2
+        # x_min, x_max = min(boundary.p1[0], boundary.p2[0]), max(boundary.p1[0], boundary.p2[0])
+        # y_min, y_max = min(boundary.p1[1], boundary.p2[1]), max(boundary.p1[1], boundary.p2[1])
+        # if x_min <= x_inter <= x_max and y_min <= y_inter <= y_max:
+        #     return x_inter, y_inter
+        # return None
+    
+        x1, y1 = self.pos
+        x2 = x1 + np.cos(self.th)
+        y2 = y1 + np.sin(self.th)
+        x3, y3 = boundary.p1
+        x4, y4 = boundary.p2
 
-            den = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
-            t = ((x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4)) / den
-            u = -((x1 - x2)*(y1 - y3) - (y1 - y2)*(x1 - x3)) / den
+        den = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
+        if den == 0: return None
+        t = ((x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4)) / den
+        u = -((x1 - x2)*(y1 - y3) - (y1 - y2)*(x1 - x3)) / den
 
-            if t > 0 and 0 <= u <= 1:
-                return (x3 + u*(x4 - x3), y3 + u*(y4 - y3))
-            return None
-        except ZeroDivisionError as e:
-            return None
+        if t > 0 and 0 <= u <= 1:
+            return (x3 + u*(x4 - x3), y3 + u*(y4 - y3))
+        return None
     
 
 class Raycaster:
@@ -72,7 +70,7 @@ class Raycaster:
         self.pos = pos
         self.rays = []
         for th in range(0, 360, 1):
-            ray = Ray((self.pos[0], self.pos[1]), math.radians(th))
+            ray = Ray((self.pos[0], self.pos[1]), np.radians(th))
             self.rays.append(ray)
 
     
@@ -119,15 +117,34 @@ for _ in range(5):
     boundaries.append(b)
 
 caster = Raycaster((WIDTH//2 , HEIGHT//2))
+caster_ax, caster_ay = 0, 0
+caster_vx, caster_vy = 0, 0
 
 running = True
+mouse_in_screen = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
         if event.type == pygame.MOUSEMOTION:
-            caster.move(event.pos)
+            if (offset < event.pos[0] < WIDTH - offset) and (offset < event.pos[1] < HEIGHT - offset):
+                mouse_in_screen = True
+                caster.move(event.pos)
+            else:
+                mouse_in_screen = False
+
+    if not mouse_in_screen:
+        caster_ax = random.uniform(-0.3, 0.3)
+        caster_ay = random.uniform(-0.3, 0.3)
+        caster_vx += caster_ax
+        caster_vy += caster_ay
+        caster.move((np.clip(caster.pos[0] + caster_vx, offset, WIDTH-offset), 
+                    np.clip(caster.pos[1] + caster_vy, offset, HEIGHT-offset)))
+        caster_vx *= 0.99
+        caster_vy *= 0.99
+    else:
+        caster_vx, caster_vy = 0, 0
 
     screen.fill('black')
 
