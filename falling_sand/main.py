fec72378bb031ffa57db2f954239aef0abda6c83
Juan Pablo Rivera <rivera.ruiz.juanp@gmail.com>
2024-12-21 18:25:48 -0600

    improved sand generation and grid update to use a next grid to store the updates rather than updating in place

--- falling_sand/main.py
+++ falling_sand/main.py
@@ -1,23 +1,46 @@
 import pygame
+import random
+
+
+def drop_sand(row, col) -> None:
+    for i in range(-DROP_RADIUS + 1, DROP_RADIUS):
+        for j in range(-DROP_RADIUS + 1, DROP_RADIUS):
+            shifted_row, shifted_col = row + i, col + j
+            if shifted_row < 0 or shifted_row >= ROWS or \
+               shifted_col < 0 or shifted_col >= COLS or \
+               ((shifted_row - row)**2 + (shifted_col - col)**2)**0.5 >= DROP_RADIUS:
+                continue
+            grid[shifted_row][shifted_col] = int(hue)
 
 
 def update_grid() -> None:
-    for row in range(ROWS-1, -1, -1):
-        for col in range(COLS-1, -1, -1):
+    global grid
+    new_grid = [[0] * COLS for _ in range(ROWS)]
+    for row in range(0, ROWS):
+        for col in range(0, COLS):
             # No sand to move
             if not grid[row][col]: continue
 
             # Sand grain can keep falling down
             if row < ROWS - 1 and grid[row+1][col] == 0:
-                grid[row][col], grid[row+1][col] = grid[row+1][col], grid[row][col]
+                new_grid[row+1][col] = grid[row][col]
 
-            # Sand grain can fall to the bottom-right side
-            elif row < ROWS - 1 and col < COLS - 1 and grid[row+1][col+1] == 0:
-                grid[row][col], grid[row+1][col+1] = grid[row+1][col+1], grid[row][col]
+            else:
+                side_options = []
+                # Sand grain can fall to the bottom-right side
+                if row < ROWS - 1 and col < COLS - 1 and grid[row+1][col+1] == 0:
+                    side_options.append((row+1, col+1))
 
-            # Sand grain can fall to the bottom-left side
-            elif row < ROWS - 1 and col > 0 and grid[row+1][col-1] == 0:
-                grid[row][col], grid[row+1][col-1] = grid[row+1][col-1], grid[row][col]
+                # Sand grain can fall to the bottom-left side
+                if row < ROWS - 1 and col > 0 and grid[row+1][col-1] == 0:
+                    side_options.append((row+1, col-1))
+
+                if not side_options: 
+                    new_grid[row][col] = grid[row][col]
+                else:
+                    side_cell = random.choice(side_options)
+                    new_grid[side_cell[0]][side_cell[1]] = grid[row][col]
+    grid = new_grid
 
 
 
@@ -34,10 +57,11 @@ def draw_cell(row: int, col: int) -> None:
     pygame.draw.rect(screen, sand_color, rect)
 
 
-WIDTH = 600
-HEIGHT = 600
-ROWS = 100
-COLS = 100
+WIDTH = 500
+HEIGHT = 500
+ROWS = 250
+COLS = 250
+DROP_RADIUS = 5
 ROW_SIZE = HEIGHT // ROWS
 COL_SIZE = WIDTH // COLS
 
@@ -68,15 +92,15 @@ while running:
         x, y = pygame.mouse.get_pos()
         event_row = y // ROW_SIZE
         event_col = x // COL_SIZE
-        grid[event_row][event_col] = int(hue)
+        drop_sand(event_row, event_col)
 
-        hue += 0.1
+        hue += 0.2
         if hue > 360: hue = 1
 
     update_grid()
     draw_grid()
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(90)
 
 pygame.quit()
